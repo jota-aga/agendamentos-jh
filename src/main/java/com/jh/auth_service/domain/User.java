7400c2b1e65e721f0912589b8d89b04a2cf14e16
joão henrique <joaolgg2estudos@gmail.com>
2026-08-11 18:51:36 -0300

    novas entidades user e user role

---
+++ src/main/java/com/jh/auth_service/domain/User.java
@@ -0,0 +1,40 @@
+package com.jh.auth_service.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Email(message = "Email não é válido")
+	@NotBlank(message = "Email não deve ser vazio")
+	private String email;
+	
+	@NotBlank(message="Nome não deve ser vazio")
+	private String nome;
+	
+	@NotBlank(message = "Senha não deve ser vazia")
+	@Size(min = 8, message = "Senha deve conter mais de 7 caracteres")
+	private String senha;
+	
+	@NotNull(message = "Role é obrigatório")
+	private UserRole role;
+}
---
+++ src/main/java/com/jh/auth_service/domain/UserRole.java
@@ -0,0 +1,39 @@
+package com.jh.auth_service.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRole {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Role nome;
+	
+	enum Role{
+		ADMIN(1L),
+		CLIENT(2L);
+		
+		private Long id;
+		
+		Role(Long id){
+			this.id = id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+	}
+}
