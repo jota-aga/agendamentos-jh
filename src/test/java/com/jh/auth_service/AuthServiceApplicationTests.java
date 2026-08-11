24d1ae26400f081e6c0e25fc23ac8866814e9f09
joão henrique <joaolgg2estudos@gmail.com>
2026-08-11 18:27:24 -0300

    organização de pacotes

---
+++ src/test/java/com/jh/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
