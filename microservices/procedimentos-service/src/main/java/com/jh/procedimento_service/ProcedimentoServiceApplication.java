0e38e56b1426316ddae07d4528a169d39c327cb5
joão henrique <joaolgg2estudos@gmail.com>
2026-08-16 03:36:52 -0300

    novo serviço e organização de seus pacotes

---
+++ microservices/procedimentos-service/src/main/java/com/jh/procedimento_service/ProcedimentoServiceApplication.java
@@ -0,0 +1,11 @@
+package com.jh.procedimento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcedimentoServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProcedimentoServiceApplication.class, args);
+	}
+}
